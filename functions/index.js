74ae6591e1e7dd4045f45666fe616b8513c7fceb
timcartwright <tim@websitesbytim.com>
2017-09-03 00:08:00 +0100

    Slack notifications for game requests

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,19 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const rp = require('request-promise');
+
 admin.initializeApp(functions.config().firebase);
 
 exports.updateLeaderboard = functions.database.ref('/matches/{pushId}')
-    .onWrite(event => {
-      const match = event.data.val();
-      let loser;
-      let winner;
-      let winnerKey;
-      const leaderboard = admin.database().ref('leaderboard');
-      let newLeaderboard = [];
+.onWrite(event => {
+    const match = event.data.val();
+    let loser;
+    let winner;
+    let winnerKey;
+    const leaderboard = admin.database().ref('leaderboard');
+    let newLeaderboard = [];
 
-      leaderboard.once('value').then(function(positions) {
+    leaderboard.once('value').then(function(positions) {
         positions.forEach(function(position) {
             const playerKey = position.val();
 
@@ -39,4 +41,50 @@ exports.updateLeaderboard = functions.database.ref('/matches/{pushId}')
         // writing to the Firebase Realtime Database.
         return leaderboard.set(newLeaderboard);
     });
+});
+
+exports.requestMatch = functions.database.ref('/players/{player}')
+.onUpdate(event => {
+    const player = event.data.val();
+    const playingState = player.playingState;
+
+    if (player.playingState !== 'has been selected') return false;
+
+    const slackToken = functions.config().slack.token;
+    const userListUrl = 'https://slack.com/api/users.list?token=' + slackToken;
+    const sendMessageUrl = 'https://slack.com/api/chat.postMessage';
+    let slackUser;
+
+    const options = {
+        url: userListUrl,
+        json: true
+    };
+
+
+    return rp(options)
+    .then(response => {
+        slackUser = response.members.find(member => member.profile.email === 'tim.cartwright@mediablazegroup.com');
+        const opponentRef = admin.database().ref('players/' + player.opponent);
+        return opponentRef.once('value');
+    })
+    .then(opponent => {
+        const opponentName = opponent.child('displayName').val();
+        console.log(opponentName);
+
+        const sendMessageOptions = {
+            method: 'POST',
+            url: sendMessageUrl,
+            formData: {
+                token: slackToken,
+                channel: '@' + slackUser.name,
+                text: '@' + slackUser.name + ' Want to touch cloth with ' + opponentName + '?'
+            }
+        };
+
+        return rp(sendMessageOptions);
+    })
+    .then(response => {
+        console.log('response:', response);
+    });
+
 });
