4f6f1ddf2d10915294e8899b63a698c22d513a40
timcartwright <tim@websitesbytim.com>
2017-09-03 12:52:55 +0100

    Leaderboard update animations

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,53 +1,38 @@
 import React from 'react';
-import styled from 'styled-components';
-import List from './List';
-import ListItem from './ListItem';
+import FlipMove from 'react-flip-move';
 import Streak from './Streak';
 import InlineFlex from './InlineFlex';
+import '../App.css';
 
 const Leaderboard = ({currentPlayer, isSelectingOpponent,leaderboard, players, selectOpponent}) => {
 
     if (!leaderboard || !players) return null;
 
-    const StyledList = styled(List)`
-        box-shadow: ${isSelectingOpponent ? '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)' : 'none'};
-        margin: 0 auto;
-        max-width: 375px;
-    `
-
-    const StyledListItem = styled(ListItem)`
-        ${props => props.link && `
-            cursor: pointer;
-
-            &:hover {
-                text-decoration: underline;
-            }
-        `}
-    `
-
     return (
-        <StyledList>
-            {leaderboard.map(playerKey => {
-                const player = players.find(p => p.key === playerKey);
-                const {displayName, key, streak, email, playingState} = player;
-                const isCurrentPlayer = key === currentPlayer.key;
-                const playerIsInactive = playingState === 'inactive';
-                const selectable = isSelectingOpponent && playerIsInactive && !isCurrentPlayer;
+        <div className={isSelectingOpponent ? 'leaderboard leaderboard--active' : 'leaderboard'}>
+            <FlipMove duration={750} easing="ease-out">
+                {leaderboard.map(playerKey => {
+                    const player = players.find(p => p.key === playerKey);
+                    const {displayName, key, streak, email, playingState} = player;
+                    const isCurrentPlayer = key === currentPlayer.key;
+                    const playerIsInactive = playingState === 'inactive';
+                    const selectable = isSelectingOpponent && playerIsInactive && !isCurrentPlayer;
 
-                return (
-                    <StyledListItem
-                        link={selectable}
-                        key={key}
-                        onClick={selectable && selectOpponent.bind(null, player)}
-                    >
-                        <span>{email} - {playingState}</span>
-                        <InlineFlex>
-                            {streak && <Streak streak={streak.slice(-5)} />}
-                        </InlineFlex>
-                    </StyledListItem>
-                )
-            })}
-        </StyledList>
+                    return (
+                        <div
+                            className={selectable ? 'leaderboard__item leaderboard__item--active' : 'leaderboard__item'}
+                            key={key}
+                            onClick={selectable && selectOpponent.bind(null, player)}
+                        >
+                            <span>{email}</span>
+                            <InlineFlex>
+                                {streak && <Streak streak={streak.slice(-5)} />}
+                            </InlineFlex>
+                        </div>
+                    )
+                })}
+            </FlipMove>
+        </div>
     );
 }
 
